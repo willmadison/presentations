6e9e80464fbee9ab489ef6412ecf225d46852f56
Will Madison <wmadisondev@gmail.com>
2017-12-19 00:11:47 -0500

    commit slides for tomorrow's lightning talks

---
+++ what-makes-go-so-great/expressive.go
@@ -0,0 +1,10 @@
+// START EXPRESSIVE OMIT
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+}
+
+// END EXPRESSIVE OMIT
---
+++ what-makes-go-so-great/verbose.java
@@ -0,0 +1,9 @@
+// START VERBOSE OMIT
+class Verbose {
+
+    public static void main(String []args) {
+        System.out.println("Hello, World!");
+    }
+}
+
+// END VERBOSE OMIT
